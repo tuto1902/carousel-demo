d4b64fa9a6554d4caf1a855bbc2fb1d59bae87e5
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2024-05-29 19:25:47 -0600

    week-1

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <main class="container flex items-center justify-center mx-auto pt-14 text-center">
+        <x-carousel::carousel />
+    </main>
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.blade.php',
+    './vendor/tuto1902/carousel/resources/**/*.blade.php'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
